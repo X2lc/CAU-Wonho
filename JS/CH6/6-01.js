8ec2c7f1d8edf27c0f526f0a2cd6548fe8c51197
Xelc <dnjsgh1023@gmail.com>
2021-08-31 18:53:59 +0900

    6-1. Person.prototype

---
+++ JS/CH6/6-01.js
@@ -0,0 +1,11 @@
+var Person = function(name) {
+    this._name = name;
+  };
+  Person.prototype.getName = function() {
+    return this._name;
+  }; 
+  this._name = name;
+  
+Person.prototype.getName = function() {
+  return this._name;
+}; 
