5f927b3cd8b76b962744b6a0a3a70682b2ef4bb6
Xelc <dnjsgh1023@gmail.com>
2021-09-08 02:15:57 +0900

    9.3.1-3 classnames - 예시 코드2

--- React/CH9/src/etc.js
+++ React/CH9/src/etc.js
@@ -1,15 +1,6 @@
-import classNames from ‘classnames’;
-
-
-classNames(‘one’, ‘two’); // = ‘one two‘
-classNames(‘one’, { two: true }); // = ‘one two‘
-classNames(‘one’, { two: false }); // = ‘one‘
-classNames(‘one’, [‘two’, ‘three’]); // = ‘one two three‘
-
-
-
-const myClass = ‘hello’;
-classNames(‘one’, myClass, { myCondition: true }); // = ‘one hello myCondition‘ 
 const MyComponent = ({ highlighted, theme }) => (
-  <div className={classNames("MyComponent", { highlighted }, theme)}>Hello</div>
-);
+    <div className={classNames("MyComponent", { highlighted }, theme)}>Hello</div>
+    <div className={`MyComponent ${theme} ${highlighted ? "highlighted" : ""}`}>
+      Hello
+    </div>
+  );
