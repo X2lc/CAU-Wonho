4cb5b3e7d2d0987e15ae098cccb0f0072f80e622
Xelc <dnjsgh1023@gmail.com>
2021-09-14 02:51:46 +0900

    13.4.1 URL 파라미터

--- React/CH13. router-tutorial/etc.js
+++ React/CH13. router-tutorial/etc.js
@@ -1,4 +1,5 @@
 const { Route } = require("react-router-dom");
+1. / profiles / velopert  // 파라미터 예시
 
-<Route path="주소규칙" component={보여 줄 컴포넌트} /> 
 <Link to="주소">내용</Link> 
+2. / about ? detales == true // 쿼리 예시 
--- React/CH13. router-tutorial/src/App.js
+++ React/CH13. router-tutorial/src/App.js
@@ -2,22 +2,25 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import About from './About'
 import Home from './Home'
+import Profile from './Profile'
+
 const App = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
+@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
-      <Route path="/info" component={About} />
       <Route path={['/about', '/info']} component={About} />
+      <Route pate="/profile/:username" component={Profile} />
 
     </div>
   )
