3cbebe50c81d63834aee3e9b328f2c7f7a1e1fe0
Xelc <dnjsgh1023@gmail.com>
2021-09-14 02:16:01 +0900

    11.4 React.memo를 사용하여 컴포넌트 성능 최적화

--- React/CH10,11/src/components/TodoListItem.js
+++ React/CH10,11/src/components/TodoListItem.js
@@ -6,14 +6,10 @@ import {
 } from 'react-icons/md';
 import cn from 'classnames';
 import './TodoListItem.scss';
-
-const TodoListItem = ({ todo, onRemove }) => {
 const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
-
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
       <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
@@ -24,4 +20,6 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
     </div>
   );
 };
+
 export default TodoListItem;
+export default React.memo(TodoListItem);
