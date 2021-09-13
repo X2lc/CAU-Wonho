3a2f7f9df3819fa80cf81335c4200f7a4baeb318
Xelc <dnjsgh1023@gmail.com>
2021-09-14 02:09:04 +0900

    10.3.4-2 TodoListItem에서 토글 함수 호출하기

--- React/CH10,11/src/components/TodoList.js
+++ React/CH10,11/src/components/TodoList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
-const TodoList = ({ todos }) => {
+
+const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} />
         <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
