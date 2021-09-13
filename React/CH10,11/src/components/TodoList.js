ebbcac32cfa6c047ee6a3ebbb3f0c76486d47bc9
Xelc <dnjsgh1023@gmail.com>
2021-09-14 02:06:56 +0900

    10.3.3-3 TodoListItem에서 삭제 함수 호출하기

--- React/CH10,11/src/components/TodoList.js
+++ React/CH10,11/src/components/TodoList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
-
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+      ))}
     </div>
   );
 };
-
 export default TodoList;
