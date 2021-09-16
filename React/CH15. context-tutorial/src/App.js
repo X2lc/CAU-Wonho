57e7a7845564c36c910f0f3a8e94bb359b30ce31
Xelc <dnjsgh1023@gmail.com>
2021-09-17 01:05:16 +0900

    15.3.2 새로워진 Context를 프로젝트에 반영하기

--- React/CH15. context-tutorial/src/App.js
+++ React/CH15. context-tutorial/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
+import { ColorProvider } from './contexts/color';
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorContext.Provider>
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
     </ColorContext.Provider>
+    </ColorProvider>
   );
 };
-
-export default App; 
 
 export default App;
