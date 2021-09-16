e70d3e94607adbee53bc48369aec93e6e9b2703d
Xelc <dnjsgh1023@gmail.com>
2021-09-17 01:03:47 +0900

    15.2.3 Provider

--- React/CH15. context-tutorial/src/App.js
+++ React/CH15. context-tutorial/src/App.js
@@ -3,12 +3,17 @@ import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
 const App = () => {
   return (
+    <div>
+      <ColorBox />
+    </div>
     <ColorContext.Provider value={{ color: 'red' }}>
-    <ColorContext.Provider>
       <div>
         <ColorBox />
       </div>
     </ColorContext.Provider>
   );
 };
+
+export default App; 
+
 export default App;
