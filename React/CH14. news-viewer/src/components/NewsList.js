a87312c48c962b8489720ab82e78f77d661fd6ab
Xelc <dnjsgh1023@gmail.com>
2021-09-17 00:46:01 +0900

    14.6.2 API를 호출 할 때 카테고리 지정하기

--- React/CH14. news-viewer/src/components/NewsList.js
+++ React/CH14. news-viewer/src/components/NewsList.js
@@ -1,10 +1,7 @@
-import React from 'react';
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
-
-
 const NewsListBlock = styled.div`
 box-sizing: border-box;
 padding-bottom: 3rem;
@@ -17,6 +14,8 @@ margin-top: 2rem;
     padding-right: 1rem;
 }
 `;
+
+
 const sampleArticle = {
     title: '제목',
     description: '내용',
@@ -25,7 +24,9 @@ const sampleArticle = {
 };
 
 
+
 const NewsList = () => {
+const NewsList = ({ category }) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -34,8 +35,10 @@ const NewsList = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`
                 const response = await axios.get(
                     'http://newsapi.org/v2/top-headlines?country=kr&apiKey=1dff0610147149da9758cbaa531976ed',
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=1dff0610147149da9758cbaa531976ed`,
                 );
                 setArticles(response.data.articles);
             } catch (e) {
@@ -45,6 +48,7 @@ const NewsList = () => {
         };
         fetchData();
     }, []);
+    }, [category]);
 
     // 대기 중일 때
     if (loading) {
@@ -54,24 +58,13 @@ const NewsList = () => {
     if (!articles) {
         return null;
     }
-
     // articles 값이 유효할 때
     return (
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
             {articles.map(article => (
                 <NewsItem key={article.url} article={article} />
             ))}
         </NewsListBlock>
     );
 };
-
-
-
-export default NewsList; 
 export default NewsList;
