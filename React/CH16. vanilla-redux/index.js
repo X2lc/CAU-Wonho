2546d0a4e21ec9c29efa16e8fa9ffc86b0311e66
Xelc <dnjsgh1023@gmail.com>
2021-09-18 01:51:46 +0900

    16.2.10 액션 발생시키기

--- React/CH16. vanilla-redux/index.js
+++ React/CH16. vanilla-redux/index.js
@@ -50,6 +50,17 @@ const render = () => {
 };
 
 
-render();
 
+render();
 store.subscrib(render);
+store.subscribe(render);
+
+divToggle.onclick = () => {
+    store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+    store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+    store.dispatch(decrease());
+};
