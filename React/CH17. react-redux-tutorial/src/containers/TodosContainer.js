d96d8a75dbfc43364620a72f8b3e5cec85bbb6ee
Xelc <dnjsgh1023@gmail.com>
2021-09-18 02:28:03 +0900

    17.7.5 useActions 유틸 Hook을 만들어서 사용하기

--- React/CH17. react-redux-tutorial/src/containers/TodosContainer.js
+++ React/CH17. react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { connect } from 'react-redux';
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
     const { input, todos } = useSelector(({ todos }) => ({
@@ -18,22 +19,14 @@ const TodosContainer = () => {
     const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
     const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
 
-const TodosContainer = ({
-    input,
-    todos,
-    changeInput,
-    insert,
-    toggle,
-    remove,
-}) => {
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    );
     return (
         <Todos
             input={input}
             todos={todos}
-            onChangeInput={changeInput}
-            onInsert={insert}
-            onToggle={toggle}
-            onRemove={remove}
             onChangeInput={onChangeInput}
             onInsert={onInsert}
             onToggle={onToggle}
@@ -41,21 +34,4 @@ const TodosContainer = ({
         />
     );
 };
-
-
-
-export default connect(
-    // 비구조화 할당을 통해 todos를 분리하여
-    // state.todos.input 대신 todos.input을 사용
-    ({ todos }) => ({
-        input: todos.input,
-        todos: todos.todos,
-    }),
-    {
-        changeInput,
-        insert,
-        toggle,
-        remove,
-    },
-)(TodosContainer); 
-export default TodosContainer; 
+export default TodosContainer;
