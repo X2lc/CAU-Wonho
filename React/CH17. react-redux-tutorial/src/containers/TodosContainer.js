bd417833ce2edd98493e222e6d6640407643b356
Xelc <dnjsgh1023@gmail.com>
2021-09-18 02:26:22 +0900

    17.7.4 TodosContainer를 Hooks로 전환하기

--- React/CH17. react-redux-tutorial/src/containers/TodosContainer.js
+++ React/CH17. react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 
+const TodosContainer = () => {
+    const { input, todos } = useSelector(({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos
+    }));
+    const dispatch = useDispatch();
+    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
+        dispatch
+    ]);
+    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
 
 const TodosContainer = ({
     input,
@@ -20,6 +34,10 @@ const TodosContainer = ({
             onInsert={insert}
             onToggle={toggle}
             onRemove={remove}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
         />
     );
 };
@@ -40,3 +58,4 @@ export default connect(
         remove,
     },
 )(TodosContainer); 
+export default TodosContainer; 
