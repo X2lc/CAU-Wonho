41be7ac0ca69e7c1f5f8e6079d8777d3df82a8f4
Xelc <dnjsgh1023@gmail.com>
2021-09-18 02:05:47 +0900

    17.4.1 스토어 만들기

--- React/CH17. react-redux-tutorial/src/modules/index.js
+++ React/CH17. react-redux-tutorial/src/modules/index.js
@@ -1,13 +1,29 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-import todos from './todos';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+const store = createStore(rootReducer);
 
 
-const rootReducer = combineReducers({
-    counter,
-    todos,
-});
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-export default rootReducer; 
+serviceWorker.unregister(); 
