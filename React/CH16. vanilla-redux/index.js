38a3b5e7070eab18792dabf7dc54d82079fbd37c
Xelc <dnjsgh1023@gmail.com>
2021-09-18 01:48:23 +0900

    16.2.5 초깃값 설정

--- React/CH16. vanilla-redux/index.js
+++ React/CH16. vanilla-redux/index.js
@@ -1,8 +1,9 @@
-const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
-const INCREASE = 'INCREASE';
-const DECREASE = 'DECREASE'; 
-const DECREASE = 'DECREASE';
-
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE }); 
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+    toggle: false,
+    counter: 0
+}; 
