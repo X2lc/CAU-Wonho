9b6fa2c8f5c1814a3edf725a25e496ccca157163
Xelc <dnjsgh1023@gmail.com>
2021-09-20 18:18:03 +0900

    18.3.1-2 미들웨어 적용하기

--- React/CH18. learn-redux-middleware/src/index.js
+++ React/CH18. learn-redux-middleware/src/index.js
@@ -6,14 +6,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
 // import loggerMiddleware from ‘./lib/loggerMiddleware‘;
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 const logger = createLogger();
 const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 
 
