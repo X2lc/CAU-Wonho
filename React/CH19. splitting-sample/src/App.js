b1e040d12214d9224aa9af0f67c99acfde7783e5
Xelc <dnjsgh1023@gmail.com>
2021-09-20 18:54:43 +0900

    19.1.2 자바스크립트 함수 비동기 로딩

--- React/CH19. splitting-sample/src/App.js
+++ React/CH19. splitting-sample/src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import notify from './notify';
+
+
 function App() {
+  const onClick = () => {
+    notify();
+    import('./notify').then(result => result.default());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello React!</p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
 }
-
 export default App;
-export default App; 
