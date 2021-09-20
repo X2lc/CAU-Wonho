f92d51d9d1184a5aa75d40f8d5f7282ca3909eb1
Xelc <dnjsgh1023@gmail.com>
2021-09-20 18:56:12 +0900

    19.2.1 state를 사용한 코드 스플리팅

--- React/CH19. splitting-sample/src/App.js
+++ React/CH19. splitting-sample/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import notify from './notify';
-
 
 function App() {
   const onClick = () => {
-    notify();
     import('./notify').then(result => result.default());
+
+class App extends Component {
+  state = {
+    SplitMe: null
+  };
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default
+    });
   };
   return (
     <div className='App'>
@@ -17,5 +25,19 @@ function App() {
       </header>
     </div>
   );
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p onClick={this.handleClick}>Hello React!</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
 }
+
+
 export default App;
