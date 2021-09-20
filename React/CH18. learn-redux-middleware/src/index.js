6d5942388115ae70decd5b0909e60300660312de
Xelc <dnjsgh1023@gmail.com>
2021-09-20 18:10:46 +0900

    18.2.1 미들웨어 만들기

--- React/CH18. learn-redux-middleware/src/index.js
+++ React/CH18. learn-redux-middleware/src/index.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
 
 const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 
 
 ReactDOM.render(
-  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </React.StrictMode>,
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-serviceWorker.unregister(); 
