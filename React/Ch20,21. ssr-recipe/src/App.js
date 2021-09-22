e3ea5af0898b2129677a519a247d963e9ef7aa93
Xelc <dnjsgh1023@gmail.com>
2021-09-23 01:56:23 +0900

    20.5.1 라우트 컴포넌트 스플리팅하기

--- React/Ch20,21. ssr-recipe/src/App.js
+++ React/Ch20,21. ssr-recipe/src/App.js
@@ -4,14 +4,17 @@ import Menu from './components/Menu';
 import RedPage from './pages/RedPage';
 import BluePage from './pages/BluePage';
 import UsersPage from './pages/UsersPage';
+import loadable from '@loadable/component';
+const RedPage = loadable(() => import('./pages/RedPage'));
+const BluePage = loadable(() => import('./pages/BluePage'));
+const UsersPage = loadable(() => import('./pages/UsersPage'));
+
+
 const App = () => {
   return (
     <div>
       <Menu />
       <hr />
-      <Route path='/red' component={RedPage} />
-      <Route path='/blue' component={BluePage} />
-      <Route path='./users' component={UsersPage} />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
       <Route path="/users" component={UsersPage} />
