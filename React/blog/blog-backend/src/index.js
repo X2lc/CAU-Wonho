cd359af36917316dab6482d7baab31811bba196d
Xelc <dnjsgh1023@gmail.com>
2021-10-10 18:17:53 +0900

    21.5.2 라우트 파라미터와 쿼리

--- React/blog/blog-backend/src/index.js
+++ React/blog/blog-backend/src/index.js
@@ -1,17 +1,22 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-
 const app = new Koa();
 const router = new Router();
-
 // 라우터 설정
 router.get('/', (ctx) => {
   ctx.body = '홈';
 });
 router.get('/about', (ctx) => {
   ctx.body = '소개';
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
 });
 
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다';
+});
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
