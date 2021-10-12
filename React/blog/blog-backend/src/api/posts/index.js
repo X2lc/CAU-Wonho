12c3133bf212cd224b04af796f9773990def7c7e
Xelc <dnjsgh1023@gmail.com>
2021-10-13 03:12:51 +0900

    22.9.1 Objectld 검증

--- React/blog/blog-backend/src/api/posts/index.js
+++ React/blog/blog-backend/src/api/posts/index.js
@@ -1,11 +1,17 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
 const posts = new Router();
+
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:id', postsCtrl.checkObjectId.read);
+posts.delete('/:id', postsCtrl.checkObjectId.remove);
+posts.patch('/:id', postsCtrl.checkObjectId.update);
 
+const post = new Router();
+posts.get('/', postsCtrl.read);
+posts.delete('/', postsCtrl.remove);
+posts.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 export default posts;
