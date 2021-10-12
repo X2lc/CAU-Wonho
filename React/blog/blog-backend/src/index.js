d107e4eb6b6b7e70524787f737755117ead872f8
Xelc <dnjsgh1023@gmail.com>
2021-10-13 02:40:39 +0900

    22.3.1 .env 환경변수 파일 생성

--- React/blog/blog-backend/src/index.js
+++ React/blog/blog-backend/src/index.js
@@ -1,19 +1,22 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+const { PORT } = process.env;
 
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
-
 // 라우터 설정
 router.use('/api', api.routes());
-
 app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
+const port = PORT || 4000;
 app.listen(4000, () => {
   console.log('Listening to port 4000');
+  console.log('Listening to port %d', port);
 });
