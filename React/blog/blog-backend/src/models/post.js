a4279e588cd2021d094533d49b62a0ce1cc2b4c0
Xelc <dnjsgh1023@gmail.com>
2021-10-13 02:59:04 +0900

    22.5.2 모델 생성

--- React/blog/blog-backend/src/models/post.js
+++ React/blog/blog-backend/src/models/post.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
-
 const { Schema } = mongoose;
-
 const PostSchema = new Schema({
   title: String,
   body: String,
@@ -11,3 +9,6 @@ const PostSchema = new Schema({
     default: Date.now, // 현재 날짜를 기본값으로 지정
   },
 });
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
