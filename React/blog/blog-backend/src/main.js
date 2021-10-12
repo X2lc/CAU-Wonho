67c48646d705fb61291f3efe4437f71e1bfcef90
Xelc <dnjsgh1023@gmail.com>
2021-10-13 02:56:11 +0900

    22.4.1 기존 코드 ES Module 형태로 바꾸기

--- React/blog/blog-backend/src/main.js
+++ React/blog/blog-backend/src/main.js
@@ -3,6 +3,12 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -18,14 +24,11 @@ const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
-
 // 라우터 설정
 router.use('/api', api.routes());
-
 app.use(bodyParser());
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
-
 const port = PORT || 4000;
 app.listen(4000, () => {
   console.log('Listening to port %d', port);
