dfa826003da96b258877e32ae997318b2f0258c0
Xelc <dnjsgh1023@gmail.com>
2021-10-19 05:03:56 +0900

    24.3.2-1 로그인 상태 보여주기

--- React/blog/blog-frontend/src/components/common/Header.js
+++ React/blog/blog-frontend/src/components/common/Header.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import Responsive from './Responsive';
 import Button from './Button';
 import { Link } from 'react-router-dom';
-
 const HeaderBlock = styled.div`
   position: fixed;
   width: 100%;
@@ -34,19 +33,34 @@ const Wrapper = styled(Responsive)`
 const Spacer = styled.div`
   height: 4rem;
 `;
+
 const Header = () => {
+const UserInfo = styled.div`
+  font-weight: 800;
+  margin-right: 1rem;
+`;
+
+const Header = ({ user }) => {
   return (
     <>
       <HeaderBlock>
         <Wrapper>
-          <div className="logo">REACTERS</div>
           <Link to="/" className="logo">
             REACTERS
           </Link>
           <div className="right">
-            <Button>로그인</Button>
             <Button to="/login">로그인</Button>
           </div>
+          {user ? (
+            <div className="right">
+              <UserInfo>{user.username}</UserInfo>
+              <Button>로그아웃</Button>
+            </div>
+          ) : (
+            <div className="right">
+              <Button to="/login">로그인</Button>
+            </div>
+          )}
         </Wrapper>
       </HeaderBlock>
       <Spacer />
