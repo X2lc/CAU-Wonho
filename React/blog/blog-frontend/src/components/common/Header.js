aa0630f4f4860b640a8d9ddb0f354fe1e4bd08b3
Xelc <dnjsgh1023@gmail.com>
2021-10-19 05:08:11 +0900

    24.3.3 로그아웃 기능 구현

--- React/blog/blog-frontend/src/components/common/Header.js
+++ React/blog/blog-frontend/src/components/common/Header.js
@@ -33,14 +33,13 @@ const Wrapper = styled(Responsive)`
 const Spacer = styled.div`
   height: 4rem;
 `;
-
-const Header = () => {
 const UserInfo = styled.div`
   font-weight: 800;
   margin-right: 1rem;
 `;
 
 const Header = ({ user }) => {
+const Header = ({ user, onLogout }) => {
   return (
     <>
       <HeaderBlock>
@@ -48,13 +47,11 @@ const Header = ({ user }) => {
           <Link to="/" className="logo">
             REACTERS
           </Link>
-          <div className="right">
-            <Button to="/login">로그인</Button>
-          </div>
           {user ? (
             <div className="right">
               <UserInfo>{user.username}</UserInfo>
               <Button>로그아웃</Button>
+              <Button onClick={onLogout}>로그아웃</Button>
             </div>
           ) : (
             <div className="right">
