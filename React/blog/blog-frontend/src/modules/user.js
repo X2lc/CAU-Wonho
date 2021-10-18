ec3416adcfa6c546e99bdaf58847b2c9a642523f
Xelc <dnjsgh1023@gmail.com>
2021-10-19 05:06:37 +0900

    24.3.2-3 로그인 검증 실패 시 정보 초기화

--- React/blog/blog-frontend/src/modules/user.js
+++ React/blog/blog-frontend/src/modules/user.js
@@ -4,26 +4,33 @@ import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
 } from '../lib/createRequestSaga';
-
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 // 회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 );
-
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+function checkFailureSaga() {
+  try {
+    localStorage.removeItem('user'); // localStorage에서 user를 제거
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
 
 const initialState = {
   user: null,
   checkError: null,
 };
-
 export default handleActions(
   {
     [TEMP_SET_USER]: (state, { payload: user }) => ({
