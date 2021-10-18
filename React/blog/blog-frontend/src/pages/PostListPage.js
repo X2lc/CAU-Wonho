516c1cd902a8f0c970a8e34a164456d45755922a
Xelc <dnjsgh1023@gmail.com>
2021-10-19 05:00:50 +0900

    24.3.1 헤더 컴포넌트 만들기

--- React/blog/blog-frontend/src/pages/PostListPage.js
+++ React/blog/blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const PostListPage = () => {
-  return <div>포스트 리스트</div>;
   return (
     <div>
       <Button>버튼</Button>
     </div>
+    <>
+      <Header />
+      <div>안녕하세요.</div>
+    </>
   );
 };
 
