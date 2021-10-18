4775e7864295c0037572939fdc0526b60927864f
Xelc <dnjsgh1023@gmail.com>
2021-10-19 04:33:04 +0900

    24.2.1 UI 준비하기

---
+++ React/blog/blog-frontend/src/components/auth/AuthTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+/**
+ * 회원가입/로그인 페이지의 레이아웃을 담당하는 컴포넌트입니다.
+ */
+
+const AuthTemplateBlock = styled.div``;
+
+const AuthTemplate = ({ childeren }) => {
+  return <AuthTemplateBlock>{childeren}</AuthTemplateBlock>;
+const AuthTemplate = ({ children }) => {
+  return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
+};
+
+export default AuthTemplate;
