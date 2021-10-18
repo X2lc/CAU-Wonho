521d8c58c849e166412cef66f538a74ba2ae7fd0
Xelc <dnjsgh1023@gmail.com>
2021-10-19 03:38:09 +0900

    23.3.2 로그인 구현하기

--- React/blog/blog-backend/src/api/auth/auth.ctrl.js
+++ React/blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -1,7 +1,5 @@
-import Joi from 'joi';
 import Joi from '@hapi/joi';
 import User from '../../models/user';
-
 /*
   POST /api/auth/register
   {
@@ -15,7 +13,6 @@ export const register = async (ctx) => {
     username: Joi.string().alphanum().min(3).max(20).required(),
     password: Joi.string().required(),
   });
-  const result = Joi.validate(ctx.request.body, schema);
   const result = schema.validate(ctx.request.body);
   if (result.error) {
     ctx.status = 400;
@@ -35,8 +32,6 @@ export const register = async (ctx) => {
     });
     await user.setPassword(password);
     await user.save();
-
-    ctx.body = user.serialize;
     ctx.body = user.serialize();
   } catch (e) {
     ctx.throw(500, e);
@@ -44,5 +39,33 @@ export const register = async (ctx) => {
 };
 
 export const login = async (ctx) => {};
+export const login = async (ctx) => {
+  const { username, password } = ctx.request.body;
+
+  // username, password가 없으면 에러 처리
+  if (!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
 export const check = async (ctx) => {};
 export const logout = async (ctx) => {};
