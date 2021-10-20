19da95b446be8bebb5f6fca045284b380715b46b
Xelc <dnjsgh1023@gmail.com>
2021-10-21 01:34:57 +0900

    26.2.2 포스트 목록 조회 API 연동하기

--- React/blog/blog-frontend/src/pages/PostListPage.js
+++ React/blog/blog-frontend/src/pages/PostListPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostList from '../components/posts/PostList';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 const PostListPage = () => {
   return (
     <>
       <HeaderContainer />
-      <div>안녕하세요.</div>
       <PostList />
+      <PostListContainer />
     </>
   );
 };
--- React/blog/blog-frontend/src/pages/PostPage.js
+++ React/blog/blog-frontend/src/pages/PostPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostViewer from '../components/post/PostViewer';
-import PostViewerContainer from '../containers/post/PostViewerContainer';
+import PostViewerContainer from '../containers/posts/PostViewerContainer';
 
 const PostPage = () => {
   return (
