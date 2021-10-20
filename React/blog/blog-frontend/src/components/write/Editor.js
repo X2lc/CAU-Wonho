4d0d944b77755b4b4950457da484541f2e824e03
Xelc <dnjsgh1023@gmail.com>
2021-10-21 01:02:19 +0900

    25.3.1 EditorContainer 만들기

--- React/blog/blog-frontend/src/components/write/Editor.js
+++ React/blog/blog-frontend/src/components/write/Editor.js
@@ -4,7 +4,6 @@ import 'quill/dist/quill.bubble.css';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
-
 const EditorBlock = styled(Responsive)`
   padding-top: 5rem;
   padding-bottom: 5rem;
@@ -31,6 +30,7 @@ const QuillWrapper = styled.div`
 `;
 
 const Editor = () => {
+const Editor = ({ title, body, onChangeField }) => {
   const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
   const quillInstance = useRef(null); // Quill 인스턴스를 설정
 
@@ -50,15 +50,32 @@ const Editor = () => {
       },
     });
   }, []);
+    // quill에 text-change 이벤트 핸들러 등록
+    // 참고: https://quilljs.com/docs/api/#events
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, oldDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onChangeTitle = (e) => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
 
   return (
     <EditorBlock>
       <TitleInput placeholder="제목을 입력하세요" />
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onChangeTitle}
+        value={title}
+      />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
     </EditorBlock>
   );
 };
-
 export default Editor;
--- React/blog/blog-frontend/src/pages/WritePage.js
+++ React/blog/blog-frontend/src/pages/WritePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Editor from '../components/write/Editor';
+import EditorContainer from '../containers/write/EditorContainer';
 import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
@@ -8,6 +9,7 @@ const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <EditorContainer />
       <TagBox />
       <WriteActionButtons />
     </Responsive>
